245c559870c41ed889d995aadf6b31c04cb0d5ca
makszdanowicz <273564@student.pwr.edu.pl>
2025-01-27 18:35:51 +0100

    add fitnesse dependency do project

---
+++ src/main/java/com/pwr/testsfitnessefixture/SetUp.java
@@ -0,0 +1,12 @@
+package com.pwr.testsfitnessefixture;
+
+import com.pwr.presenter.AssignmentFacade;
+import fit.Fixture;
+public class SetUp extends Fixture{
+
+
+    static AssignmentFacade assignmentFacade;
+    public SetUp(){
+        // assignmentFacade = new AssignmentFacade();
+    }
+}
